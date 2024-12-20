1bf338523aa338a3b540b2d0b0eeb0872e251dd7
cisse000 <salimata2023etude@gmail.com>
2024-12-20 15:05:47 +0100

    TP BRODARD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Router, RouterLink, RouterOutlet} from '@angular/router';
+import {CookieComponent} from './cookie/cookie.component';
+import {TatoueurComponent} from './components/tatoueur/tatoueur.component';
+import {MatSidenav, MatSidenavContainer, MatSidenavContent} from '@angular/material/sidenav';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatIcon} from '@angular/material/icon';
+import {MatListItem, MatNavList} from '@angular/material/list';
+import {HttpClient} from '@angular/common/http';
+import {AuthService} from './sevices/auth.service';
+
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet,  MatSidenavContent, RouterLink, MatToolbar, MatIcon, MatListItem, MatNavList, MatSidenav, MatSidenavContainer],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'Chap2Angularr';
+  title = 'chez cisse';
+  date = new Date();
+//Ce code appelle notre service d’authentification et récupère un booléen : si l’user
+// est connecté ou non.
+  constructor(private auth: AuthService, private http: HttpClient, private router: Router) {
+  }
+  authenticated() { return this.auth.authenticated; }
+
+  logout(){this.auth.logout();}
 }
+
+
+
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import {TatoueurComponent} from './components/tatoueur/tatoueur.component';
+import {LoginComponent} from './components/login/login.component';
+import {TatoueurViewComponent} from './components/tatoueur/tatoueur-view/tatoueur-view.component';
+import {TatoueurCreateComponent} from './components/tatoueur/tatoueur-create/tatoueur-create.component';
+import {TatoueurEditComponent} from './components/tatoueur/tatoueur-edit/tatoueur-edit.component';
+export const routes: Routes = [
+  {
+    path: 'tatoueur',
+    title: 'Nos tatoueurs',
+    component: TatoueurComponent,
+  },
+  {
+    path: 'tatoueur/:tatoueurId/view',
+    component: TatoueurViewComponent
+  },
+  {
+    path: 'tatoueur/create',
+    component: TatoueurCreateComponent
+  },
+  {
+    path: 'tatoueur/:tatoueurId/edit',
+    component: TatoueurEditComponent
+  },
+  {
+    path: 'login',
+    title: 'Se connecter',
+    component: LoginComponent,
+  },
+];
